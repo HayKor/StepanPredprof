7b0735bbb0518ca73b5f30950b68017f5a9142ca
Arthur <baginian.artur@yandex.ru>
2025-03-16 13:17:59 +0300

    some commit idk

--- app/lib/logic/get_map.py
+++ app/lib/logic/get_map.py
@@ -1,25 +1,31 @@
-import sys
-import requests
 import numpy as np
+import requests
 
-def fetch_tile(url):
+from ..database import *
+
+
+def fetch_tile(url: str):
     response = requests.get(url)
     data = response.json()
-    return data['message']['data']
+    return data["message"]["data"]
 
-def generate_map():
+
+def generate_map(url: str):
     map_array = np.zeros((256, 256), dtype=int)
-    API_URL = "https://olimp.miet.ru/ppo_it/api"
-    
+
     for i in range(4):
         for j in range(4):
-            tile = fetch_tile(API_URL)
+            tile = fetch_tile(url)
             start_i = i * 64
             start_j = j * 64
-            map_array[start_i:start_i+64, start_j:start_j+64] = tile
-    
+            map_array[start_i : start_i + 64, start_j : start_j + 64] = tile
+    # Сохраняем в базу данных
+    addMatrix(map_array)
     return map_array
 
+
 if __name__ == "__main__":
-    result = generate_map()
+    API_URL = "https://olimp.miet.ru/ppo_it/api"
+    result = generate_map(API_URL)
     print(result)
+
