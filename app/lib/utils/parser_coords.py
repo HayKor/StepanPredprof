f659c37ba977260a606e145fc809bba8f5e73fad
Arthur <baginian.artur@yandex.ru>
2025-03-16 13:20:34 +0300

    add saving to db

--- app/lib/utils/parser_coords.py
+++ app/lib/utils/parser_coords.py
@@ -1,24 +1,32 @@
-import sys
 import requests
-from random import random
+
+from app.lib.database import addCoords
 
 
 API_URL = "https://olimp.miet.ru/ppo_it/api/coords"
 
+
 def pars_sender(url):
     response = requests.get(url)
     data = response.json()
-    return data['message']['listener']
+    return data["message"]["listener"]
+
 
 def pars_listener(url):
     response = requests.get(url)
     data = response.json()
-    return data['message']['price']
+    return data["message"]["price"]
+
 
 def pars_price(url):
     response = requests.get(url)
     data = response.json()
-    return data['message']['sender']
+    return data["message"]["sender"]
+
+
+def parse_all_and_save(url):
+    addCoords(pars_sender(url), pars_listener(url), pars_price(url))
+
 
 print(pars_sender(API_URL))
 print(pars_listener(API_URL))
