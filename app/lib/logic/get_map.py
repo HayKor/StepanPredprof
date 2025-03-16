47dad552fb7faa7d1fa1f293e684043382017137
VladG <gricenkovlad007@gmail.com>
2025-03-16 13:04:24 +0300

    Create get_map

--- app/lib/logic/get_map.py
+++ app/lib/logic/get_map.py
@@ -1,11 +1,25 @@
-from app.lib.utils.parser_map import fetch_tile
+import sys
+import requests
+import numpy as np
+
+def fetch_tile(url):
+    response = requests.get(url)
+    data = response.json()
+    return data['message']['data']
 
 def generate_map():
-    map = [[]*64]
-    print(tile)
-    for i in range(16):
-        for j in range(16):
-            tile = fetch_tile()
-            for x in range(64):
-                map[i][j].append(*tile[x])
-    return map
+    map_array = np.zeros((256, 256), dtype=int)
+    API_URL = "https://olimp.miet.ru/ppo_it/api"
+    
+    for i in range(4):
+        for j in range(4):
+            tile = fetch_tile(API_URL)
+            start_i = i * 64
+            start_j = j * 64
+            map_array[start_i:start_i+64, start_j:start_j+64] = tile
+    
+    return map_array
+
+if __name__ == "__main__":
+    result = generate_map()
+    print(result)
