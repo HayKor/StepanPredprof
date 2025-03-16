bd45d9e44425c8507746fd6e3621764589c43955
VladG <gricenkovlad007@gmail.com>
2025-03-16 14:24:05 +0300

    Image

--- app/lib/logic/find_pics.py
+++ app/lib/logic/find_pics.py
@@ -37,22 +37,46 @@ def find(map):
                 res.append((j, i, int(map[i][j])))
     return res
 
-if __name__ == "__main__":
+def pars_sender(url):
+    response = requests.get(url)
+    data = response.json()
+    return data["message"]["sender"]
+
+
+def pars_listener(url):
+    response = requests.get(url)
+    data = response.json()
+    return data["message"]["listener"]
+
+
+def draw():
     map = generate_map()
     x = np.arange(0, 256, 1)
     y = np.arange(0, 256, 1)
     X, Y = np.meshgrid(x, y)
     Z = map
+    API_URL = "https://olimp.miet.ru/ppo_it/api/coords"
+
+    dot_first = pars_listener(API_URL)
+    dot_second = pars_sender(API_URL)
 
     fig = plt.figure(figsize=(10, 8))
     ax = fig.add_subplot(111, projection='3d')
 
     surf = ax.plot_surface(X, Y, Z, cmap=cm.coolwarm,
                           linewidth=0, antialiased=False)
-    
+    pics = find(map)
+    # Добавляем зеленые точки для dot_first и dot_second
+    ax.scatter(dot_first[0], dot_first[1], 255, color='green', s=200)
+    ax.scatter(dot_second[0], dot_second[1], 255, color='green', s=200)
+    for x in range(len(pics)):
+        ax.scatter(pics[x][0], pics[x][1], pics[x][2], color='yellow', s=50)
     ax.set_xlabel('X')
     ax.set_ylabel('Y')
     ax.set_zlabel('Высота')
     fig.colorbar(surf, shrink=0.5, aspect=5)
 
     plt.show()
+
+if __name__ == "__main__":
+    draw()
